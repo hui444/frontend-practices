045d85a3979538eb0bb5def825bf720c9e56430e
hui444 <huihui.uni@yahoo.com>
2021-11-24 01:13:45 +0800

    feat: create (order summary) component with dynamic info

--- src/components/frontendMentor/orderSummary/orderSummaryCard.tsx
+++ src/components/frontendMentor/orderSummary/orderSummaryCard.tsx
@@ -1,21 +1,26 @@
-//started at 1030
-
-import cardIllustration from '../../../assets/frontendMentorInfo/order-summary-component-main/images/illustration-hero.svg';
 import musicIcon from '../../../assets/frontendMentorInfo/order-summary-component-main/images/icon-music.svg';
 
-const OrderSummaryCard = () => {
+interface Card {
+  illustration: string;
+  altText: string;
+  description: string;
+  planName: string;
+  costPerYear: number;
+}
+
+const OrderSummaryCard = (card: Card) => {
   return (
     <div className="os-card__container">
       <div className="os-illustration__container">
-        <img src={cardIllustration} alt="illustration of a girl listening to music and dancing" />
+        <img src={card.illustration} alt={card.altText} />
       </div>
       <div className="os-content__container">
         <h3>Order Summary</h3>
-        <p>You can now listen to millions of songs, audiobooks, and podcasts on any device anywhere you like!</p>
+        <p>{card.description}</p>
         <div className="os-plan-details__container">
           <img className="music-icon" src={musicIcon} alt="music icon" />
-          <span className="plan-text">Annual Plan</span>
-          <span className="cost-text">$59.99/year</span>
+          <span className="plan-text">{card.planName}</span>
+          <span className="cost-text">${card.costPerYear}/year</span>
           <a className="change-button">Change</a>
         </div>
         <div className="os-buttons__container">
