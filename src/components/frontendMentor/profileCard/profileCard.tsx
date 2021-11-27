06e72b039a786dac9fc662b27f12c66a460c6ff7
hui444 <huihui.uni@yahoo.com>
2021-11-28 04:14:59 +0800

    feat: complete profile card challenge

--- src/components/frontendMentor/profileCard/profileCard.tsx
+++ src/components/frontendMentor/profileCard/profileCard.tsx
@@ -1,5 +1,36 @@
+import banner from '../../../assets/frontendMentorInfo/profile-card-component-main/images/bg-pattern-card.svg';
+import avatar from '../../../assets/frontendMentorInfo/profile-card-component-main/images/image-victor.jpg';
+
 const ProfileCard = () => {
-  return <div></div>;
+  return (
+    <div className="pc-card__container">
+      <img className="pc-card__banner" src={banner} alt="banner filled with bubbles" />
+      <img className="pc-card__avatar" src={avatar} alt="user avatar" />
+      <div className="pc-card__content-container">
+        <div className="pc-card__profile-container">
+          <h3>
+            Victor Crest <span>26</span>
+          </h3>
+          <p>London</p>
+        </div>
+        <hr />
+        <div className="pc-card__stats-container">
+          <div>
+            <span className="pc-card__value">80K</span>
+            <span className="pc-card__title">Followers</span>
+          </div>
+          <div>
+            <span className="pc-card__value">803K</span>
+            <span className="pc-card__title">Likes</span>
+          </div>
+          <div>
+            <span className="pc-card__value">1.4K</span>
+            <span className="pc-card__title">Photos</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProfileCard;
--- src/pages/frontendMentor/profileCardChallenge/index.tsx
+++ src/pages/frontendMentor/profileCardChallenge/index.tsx
@@ -1,5 +1,25 @@
+import ProfileCard from '../../../components/frontendMentor/profileCard/profileCard';
+import backgroundTopCircle from '../../../assets/frontendMentorInfo/profile-card-component-main/images/bg-pattern-top.svg';
+import backgroundBottomCircle from '../../../assets/frontendMentorInfo/profile-card-component-main/images/bg-pattern-bottom.svg';
+
 const ProfileCardChallenge = () => {
-  return <div></div>;
+  return (
+    <div className="pc-challenge__page">
+      <img
+        className="pc-background-circle--top"
+        src={backgroundTopCircle}
+        alt="circle with slight gradient (top)"
+        aria-hidden
+      />
+      <img
+        className="pc-background-circle--bottom"
+        src={backgroundBottomCircle}
+        alt="circle with slight gradient (bottom)"
+        aria-hidden
+      />
+      <ProfileCard />
+    </div>
+  );
 };
 
 export default ProfileCardChallenge;
